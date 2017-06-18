abb29d8d51f16dea3950205729c9d7b10ed145a7
dev3 <yosef@surplusr-us.com>
2017-06-18 15:54:23 -0400

    Finished rma_summary_result

--- application/views/posts/rma_summary_result.php
+++ application/views/posts/rma_summary_result.php
@@ -10,6 +10,47 @@
                 <header class="rma-page-title-container">
                     <h1 class="rma-page-title"><?php echo $title ?></h1>
                 </header>
+                <div class="rma-summary-content-container">
+                    <form>
+                        <table width="100%" class="rma-summary-head rma-summary-table" cellspacing="3" border="1">
+                            <thead>
+                                <tr>
+                                    <th colspan="6" class="rma-summerry">Current RMA/Claim Summary</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td colspan="6" style="padding: 10px 5px; font-weight: bold;">Total: 1</td>
+                                </tr>
+                            </tbody>
+                            <thead>
+                                <tr>
+                                    <th class="th-background"></th>
+                                    <th class="th-background">RMA #</th>
+                                    <th class="th-background">Refund #</th>
+                                    <th class="th-background">Reason</th>
+                                    <th class="th-background">RMA Creation Date</th>
+                                    <th class="th-background">Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>
+                                        <a style="color: #1A1BCF;" href="#">RMA-595593</a>
+                                    </td>
+                                    <td>-</td>
+                                    <td>No Longer Need or Wanted - Opened</td>
+                                    <td>6/18/2017</td>
+                                    <td>Accepted</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </form>
+                    <div class="push-30-t">
+                        <a style="color: #1A1BCF; text-decoration: underline; font-weight: bold; display: block;" href="check_status">Return to RMA Search</a>
+                    </div>
+                </div>
             </article>
         </div>
     </div>
