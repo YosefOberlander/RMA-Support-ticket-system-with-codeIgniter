cab75a2b1b5336321210b2b6608fb27976380fd9
Yosef <yosef@surplusr-us.com>
2017-06-18 13:41:03 -0400

    finsihed current_rma_summary

--- application/views/posts/current_rma_summary.php
+++ application/views/posts/current_rma_summary.php
@@ -39,28 +39,41 @@
                     <br />
                     <table width="100%" class="rma-status-details" border="1">
                         <thead>
-                            <tr class="rma-status-three">
+                            <tr class="rma-status-one">
                                 <th class="status-one" rowspan="2">SKU</th>
                                 <th class="status-one" rowspan="2">Product</th>
                                 <th class="status-one" rowspan="2">Reason</th>
-                                <th class="status-one" rowspan="3">Quantity</th>
-                            </tr>
-                            <tr class="rma-status-three">
-                                <th class="status-one">Received</th>
-                                <th class="status-one">Open</th>
-                                <th class="status-one">RMA</th>
+                                <th class="status-one" rowspan="2">Quantity</th>
+                                <th class="status-one" rowspan="3">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>VE278H</td>
                                 <td>Asus VE278H 27" LED LCD Monitor</td>
-                                <td>0</td>
+                                <td>Sales Error</td>
                                 <td>1</td>
-                                <td>1</td>
+                                <td style="padding: 5px;">
+                                    <div class="dropdown">
+                                        <button class="btn btn-primary dropdown-toggle Techblue" type="button" data-toggle="dropdown">Download Label and Instructions
+                                            <span class="caret"></span></button>
+                                        <ul class="dropdown-menu" style="left: 100px; right: 100px;">
+                                            <li><a href="#"></a></li>
+                                            <li><a href="#"><i class="glyphicon glyphicon-save" style="margin-right: 5px;"></i> Download Shipping Label</a></li>
+                                            <li><a href="#"><i class="glyphicon glyphicon-envelope" style="margin-right: 5px;"></i> Email Shipping Label</a></li>
+                                            <li><a href="#"><i class="glyphicon glyphicon-file" style="margin-right: 5px;"></i> Download RMA Authorization</a></li>
+                                            <li><a href="#"><i class="glyphicon glyphicon-info-sign" style="margin-right: 5px;"></i> Download Instructions</a></li>
+                                        </ul>
+                                    </div>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
+                    <div class="push-20-t">
+                        <p class="very-rma-under-table-p">If your RMA is Pending no actions will be available</p>
+                        <a style="color: #1A1BCF; text-decoration: underline; font-weight: bold; display: block;" href="check_status">Return to RMA Search</a>
+                        <a style="color: #1A1BCF; text-decoration: underline; font-weight: bold;" href="#">Return to Summary Result</a>
+                    </div>
                 </div>
             </article>
         </div>
